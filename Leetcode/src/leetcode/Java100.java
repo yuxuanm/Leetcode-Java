722af1fd7af07154a20c8ce055a1772fcdebf2b5
yuxuanm <48812435+yuxuanm@users.noreply.github.com>
2019-10-17 23:41:30 +1100

    1

---
+++ Leetcode/src/leetcode/Java100.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+public class Java100 {
+	public static void main(String[] args) {
+		TreeNode p = new TreeNode(1);
+
+		p.left = new TreeNode(1);
+		p.right = new TreeNode(3);
+		p.left.left=new TreeNode(1);
+		
+		TreeNode q = new TreeNode(1);
+		q.left = new TreeNode(1);
+		q.right = new TreeNode(2);
+		q.left.left=new TreeNode(1);
+		
+		boolean b = isSameTree(p, q);
+		System.out.println(b);
+	}
+
+	public static boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null & q == null)
+			return true;
+		if (p == null & q != null)
+			return false;
+		if (p != null & q == null)
+			return false;
+		if (p.val != q.val)
+			return false;
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	}
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
