860807fe13d2db950e22958ba6b5cd63e7a3b74c
yuxuanm <48812435+yuxuanm@users.noreply.github.com>
2019-11-04 23:52:45 +1100

    m

---
+++ Leetcode/src/leetcode/Java141.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class Java141 {
+	public boolean hasCycle(ListNode head) {
+		
+		return false;
+	}
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
