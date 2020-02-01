8f09b1b2679478839d4e595cf79a3bddc918fc6f
yuxuanm <48812435+yuxuanm@users.noreply.github.com>
2020-02-01 23:00:26 +0800

    1 Feb 2020

--- Leetcode/src/leetcode/Q39.java
+++ Leetcode/src/leetcode/Q39.java
@@ -6,21 +6,9 @@ import java.util.List;
 
 public class Q39 {
 	public static void main(String[] args) {
-		int[] candidates = new int[] { 1,2};
+		int[] candidates = new int[] {5,10,8,4,3,12,9};
 		Q39 q39 = new Q39();
-		List<List<Integer>> res = q39.combinationSum(candidates, 4);
-		
-		ArrayList<Integer> l1 = new ArrayList<Integer>();
-		ArrayList<Integer> l2 = new ArrayList<Integer>();
-		l1.add(1);
-		l1.add(1);
-		l1.add(2);
-		
-		l2.add(2);
-		l2.add(2);
-		l2.add(2);
-		l2.add(2);
-		System.out.println(l1.containsAll(l2));
+		List<List<Integer>> res = q39.combinationSum(candidates, 27);
 	}
 
 	// DFS
@@ -35,29 +23,27 @@ public class Q39 {
 			sum = i;
 			list = new ArrayList<Integer>();
 			list.add(i);
-			subRes(candidates, sum, target, list, res);
+			subRes(candidates, sum, target, list, res,i);
 		}
 		if (!res.isEmpty())
 			return res;
 		return null;
 	}
 
-	public void subRes(int[] candidates, int sum, int target, List<Integer> list, List<List<Integer>> res) {
+	public void subRes(int[] candidates, int sum, int target, List<Integer> list, List<List<Integer>> res, int curValue) {
 		if (sum == target) {
-			// remove duplicated results
-			for (List<Integer> l : res) {
-				if (l.containsAll(list))
-					return;
-			}
 			res.add(new ArrayList<Integer>(list));
 			return;
 		}
 
 		for (int i = 0; i < candidates.length; i++) {
+			if(candidates[i]<curValue) // as we already rank the candidates, if current value is smaller than previous value, 
+									   // return to remove duplicated combination
+				continue;
 			if (sum + candidates[i] > target)
 				return;
 			list.add(candidates[i]);
-			subRes(candidates, sum + candidates[i], target, list, res);
+			subRes(candidates, sum + candidates[i], target, list, res,candidates[i]);
 			list.remove(list.size() - 1);
 		}
 	}
