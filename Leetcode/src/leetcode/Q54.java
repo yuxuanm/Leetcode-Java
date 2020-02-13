2053e943f38c7fcdf6d2ba299000d083b10e921d
yuxuanm <48812435+yuxuanm@users.noreply.github.com>
2020-02-13 23:51:01 +1100

    13 Feb 2020

--- Leetcode/src/leetcode/Q54.java
+++ Leetcode/src/leetcode/Q54.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class Q54 {
 	public static void main(String[] args) {
 //		int [][] matrix=new int [][] {new int[] {1,2,3,4},new int[] {5,6,7,8},new int[] {9,10,11,12}};
-//		int [][] matrix=new int [][] {new int[] {1,2,3},new int[] {4,5,6},new int[] {7,8,9}};
+		int [][] matrix=new int [][] {new int[] {1,2,3},new int[] {4,5,6},new int[] {7,8,9}};
 
-		int[][] matrix = new int[][] { new int[] {6,9,7}};
+//		int[][] matrix = new int[][] { new int[] { 6, 9, 7 } };
 		Q54 q54 = new Q54();
 		List<Integer> res = q54.spiralOrder(matrix);
 	}
@@ -78,32 +78,71 @@ public class Q54 {
 //		}
 //		return res;
 //	}
+	/**add the first row to the result list, then delete the first row, 
+	 * rotate the matrix, repeat the process
+	 * */
 	public List<Integer> spiralOrder(int[][] matrix) {
-		List<Integer> res = new ArrayList<Integer>();
-		if(matrix.length==0)
-			return res;
-		int top=0;
-		int bottom=matrix.length-1;
-		int right = matrix[0].length-1;
-		int left = 0;
-		while(top<=bottom&&left<=right) {
-			for(int i=left;i<=right;i++) {
-				res.add(matrix[top][i]);
+		List<Integer> ans = new ArrayList<Integer>();
+		if (matrix.length == 0)
+			return ans;
+		while (true) {
+			for (int i = 0; i < matrix[0].length; i++) {
+				ans.add(matrix[0][i]);
 			}
-			top++;
-			for(int i=top;i<=bottom;i++) {
-				res.add(matrix[i][right]);
-			}
-			right--;
-			for(int i=right;i>=left&&top<=bottom;i--) {
-				res.add(matrix[bottom][i]);
-			}
-			bottom--;
-			for(int i=bottom;i>=top&&left<=right;i--) {
-				res.add(matrix[i][left]);
-			}
-			left++;
+
+			matrix = copyArray(matrix);
+			if (matrix.length == 0)
+				break;
+			matrix = rotateArray(matrix);
 		}
-		return res;
+		return ans;
 	}
+
+	public int[][] copyArray(int[][] matrix) {
+		int[][] arr = new int[matrix.length - 1][matrix[0].length];
+		for (int i = 1; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				arr[i - 1][j] = matrix[i][j];
+			}
+		}
+		return arr;
+	}
+
+	public int[][] rotateArray(int[][] matrix) {
+		int[][] arr = new int[matrix[0].length][matrix.length];
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				arr[arr.length-1-j][i] = matrix[i][j];
+			}
+		}
+		return arr;
+	}
+//	public List<Integer> spiralOrder(int[][] matrix) {
+//		List<Integer> res = new ArrayList<Integer>();
+//		if(matrix.length==0)
+//			return res;
+//		int top=0;
+//		int bottom=matrix.length-1;
+//		int right = matrix[0].length-1;
+//		int left = 0;
+//		while(top<=bottom&&left<=right) {
+//			for(int i=left;i<=right;i++) {
+//				res.add(matrix[top][i]);
+//			}
+//			top++;
+//			for(int i=top;i<=bottom;i++) {
+//				res.add(matrix[i][right]);
+//			}
+//			right--;
+//			for(int i=right;i>=left&&top<=bottom;i--) {
+//				res.add(matrix[bottom][i]);
+//			}
+//			bottom--;
+//			for(int i=bottom;i>=top&&left<=right;i--) {
+//				res.add(matrix[i][left]);
+//			}
+//			left++;
+//		}
+//		return res;
+//	}
 }
