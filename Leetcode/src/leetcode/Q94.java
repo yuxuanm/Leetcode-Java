b768b1146911843bf2a600a2f68cad9dcc30594f
yuxuanm <48812435+yuxuanm@users.noreply.github.com>
2020-03-05 11:41:34 +1100

    5 Mar 2020

--- Leetcode/src/leetcode/Q94.java
+++ Leetcode/src/leetcode/Q94.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Q94 {
 	public static void main(String[] args) {
@@ -12,31 +13,52 @@ public class Q94 {
 		List<Integer> res = inorderTraversal(node);
 	}
 	
-	/**Method 1: recursive
+	/**Method 2: iterate
 	 * 
-	 * 0 ms beats 100%
-	 * 37.8 MB beats 5.08%
 	 * */
 	public static List<Integer> inorderTraversal(TreeNode root) {
 		List<Integer> res = new ArrayList<Integer>();
 		if (root == null)
 			return res;
-		helper(root, res);
-
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		TreeNode node=root;
+		while(node!=null||!stack.isEmpty()) {
+			while(node!=null) {
+				stack.push(node);
+				node=node.left;
+			}
+			node=stack.pop();
+			res.add(node.val);
+			node=node.right;
+		}
+		
 		return res;
 	}
-
-	private static void helper(TreeNode node, List<Integer> res) {
-		if (node != null) {
-			if (node.left != null)
-				helper(node.left, res);
-			
-			res.add(node.val);
-			
-			if (node.right != null)
-				helper(node.right, res);
-		}
-	}
+	/**Method 1: recursive
+	 * 
+	 * 0 ms beats 100%
+	 * 37.8 MB beats 5.08%
+	 * */
+//	public static List<Integer> inorderTraversal(TreeNode root) {
+//		List<Integer> res = new ArrayList<Integer>();
+//		if (root == null)
+//			return res;
+//		helper(root, res);
+//
+//		return res;
+//	}
+//
+//	private static void helper(TreeNode node, List<Integer> res) {
+//		if (node != null) {
+//			if (node.left != null)
+//				helper(node.left, res);
+//			
+//			res.add(node.val);
+//			
+//			if (node.right != null)
+//				helper(node.right, res);
+//		}
+//	}
 
 	// wrong, this method is for preorder traversal
 //	public static List<Integer> inorderTraversal(TreeNode root) {
