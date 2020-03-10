c83383d5249a89b5031c3b201ad27e3ad0f862f6
yuxuanm <48812435+yuxuanm@users.noreply.github.com>
2020-03-10 23:47:10 +1100

    10 Mar 2020

---
+++ Leetcode/src/leetcode/Q98.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q98 {
+	public static void main(String[] args) {
+		TreeNode root = TreeNode.constructTree(new Integer[] {5,1,4,null,null,3,6});
+		System.out.println(isValidBST(root));
+	}
+	public static boolean isValidBST(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        list = inOrderTraversal(root, list);
+        if(list.size()<=1)
+            return true;
+        int cur = list.get(0).intValue();
+        for(int i=1;i<list.size();i++){
+            if(list.get(i)<cur)
+                return false;
+            cur=list.get(i);
+        }
+        return true;
+        
+    }
+    
+    private static List<Integer> inOrderTraversal(TreeNode node,List<Integer> list){
+        if(node.left!=null)
+            inOrderTraversal(node.left,list);
+        list.add(node.val);
+        if(node.right!=null)
+            inOrderTraversal(node.right,list);
+        return list;
+    }
+}
