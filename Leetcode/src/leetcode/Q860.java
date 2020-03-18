f93da8949caef5258640c44b92d6143c9315db0a
yuxuanm <48812435+yuxuanm@users.noreply.github.com>
2020-03-19 01:30:42 +1100

    19 Mar 2020

---
+++ Leetcode/src/leetcode/Q860.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class Q860 {
+	// from leetcode, clever method
+	public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+		return (Math.min(rec1[2], rec2[2]) > Math.max(rec1[0], rec2[0])
+				&& Math.min(rec1[3], rec2[3]) > Math.max(rec1[1], rec2[1]));
+	}
+}
