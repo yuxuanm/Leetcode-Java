fca7a596a069e0aef5d546c9bce68e3158795f5c
yuxuanm <48812435+yuxuanm@users.noreply.github.com>
2020-04-20 00:47:01 +1000

    20 Apr 2020

---
+++ Leetcode/src/leetcode/Q1.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q1 {
+//	/**brute force
+//	 * */
+//	public int[] twoSum(int[] nums, int target) {
+//		for (int i = 0; i < nums.length - 1; i++) {
+//			for (int j = i + 1; j < nums.length; j++) {
+//				if (nums[i] + nums[j] == target)
+//					return new int[] { i, j };
+//			}
+//		}
+//		return null;
+//	}
+
+	// hash map
+	public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int remain;
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i]))
+                return new int [] {map.get(target-nums[i]),i};
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+}
