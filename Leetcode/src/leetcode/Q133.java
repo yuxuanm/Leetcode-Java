e595d6ce8a0943539ca07b3bbe90c4e819b186fb
yuxuanm <48812435+yuxuanm@users.noreply.github.com>
2020-04-27 13:34:00 +1000

    27 Apr 2020

--- Leetcode/src/leetcode/Q133.java
+++ Leetcode/src/leetcode/Q133.java
@@ -60,22 +60,22 @@ public class Q133 {
 	}
 }
 
-class Node {
-	public int val;
-	public List<Node> neighbors;
-
-	public Node() {
-		val = 0;
-		neighbors = new ArrayList<Node>();
-	}
-
-	public Node(int _val) {
-		val = _val;
-		neighbors = new ArrayList<Node>();
-	}
-
-	public Node(int _val, ArrayList<Node> _neighbors) {
-		val = _val;
-		neighbors = _neighbors;
-	}
-}
+//class Node {
+//	public int val;
+//	public List<Node> neighbors;
+//
+//	public Node() {
+//		val = 0;
+//		neighbors = new ArrayList<Node>();
+//	}
+//
+//	public Node(int _val) {
+//		val = _val;
+//		neighbors = new ArrayList<Node>();
+//	}
+//
+//	public Node(int _val, ArrayList<Node> _neighbors) {
+//		val = _val;
+//		neighbors = _neighbors;
+//	}
+//}
