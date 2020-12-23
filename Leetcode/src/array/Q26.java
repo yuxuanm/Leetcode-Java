35ee4166cd972fcf2634af224b8cfdfcec0b3d8a
yuxuanm <48812435+yuxuanm@users.noreply.github.com>
2020-12-23 23:22:56 +0800

    Create Q26.java

---
+++ Leetcode/src/array/Q26.java
@@ -0,0 +1,26 @@
+package array;
+
+public class Q26 {
+	
+	/*
+	 * Method 1
+	 * 
+	 * Two pointers, one faster pointer iterate the whole array.
+	 * One slow pointer to indicate the current non-dupilicate position.
+	 * 
+	 * Results: runtime 1ms beats 83.92% memory 40.3mb beats 59.04%.
+	 * */
+	public int removeDuplicates(int[] nums) {
+        if(nums.length==0|| nums.length==1)
+            return nums.length;
+        int fast = 0; // iterate the whole array
+        int slow = 0; // indicate the current position without duplication
+        while(fast<=nums.length-1){ // end condition
+            if(nums[slow] == nums[fast])
+                fast++;
+            else
+                nums[++slow] = nums[fast];
+        }
+        return slow+1;
+    }
+}
