0e1d03ea9663c7f146a56eebd812202e84c6f3fc
yuxuanm <48812435+yuxuanm@users.noreply.github.com>
2021-01-07 00:17:46 +0800

    Update Q33SearchInsertPosition.java

--- Leetcode/src/array/Q33SearchInsertPosition.java
+++ Leetcode/src/array/Q33SearchInsertPosition.java
@@ -1,6 +1,36 @@
 package array;
 
 public class Q33SearchInsertPosition {
+	/*
+	 * Method 2:
+	 * 
+	 * Binary search
+	 * 
+	 * Result: runtime 0ms beats 100% memroy 38mb beats 82.07%.
+	 */
+	public int searchInsert2(int[] nums, int target) {
+		int left = 0;
+		int right = nums.length - 1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (nums[mid] == target)
+				return mid;
+			else if (nums[mid] > target)
+				right = mid - 1;
+			else
+				left = mid + 1;
+		}
+		return left;
+	}
+
+	/*
+	 * Method 1:
+	 * 
+	 * Binary search + brute force
+	 * 
+	 * Result: runtime 0ms beats 100% memroy 38mb beats 75.63%. Improve: there is no
+	 * need to search insert position using brute force.
+	 */
 	public int searchInsert(int[] nums, int target) {
 		int left = 0;
 		int right = nums.length - 1;
@@ -13,16 +43,16 @@ public class Q33SearchInsertPosition {
 			else
 				left = mid + 1;
 		}
-		return insert(nums, target);
+		return insert(nums, target); // brute force to find insert position.
 	}
 
 	private int insert(int[] nums, int target) {
 		int fast = 1;
 		int slow = 0;
-		while (nums[slow] < target && nums[fast] < target || fast == nums.length) {
+		while (fast != nums.length && nums[slow] < target && nums[fast] < target) {
 			slow++;
 			fast++;
 		}
-		return fast;
+		return nums[slow] < target ? fast : slow;
 	}
 }
