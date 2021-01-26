89df973856e8243c651901f183bbd8732692a99f
yuxuanm <48812435+yuxuanm@users.noreply.github.com>
2021-01-27 00:03:26 +0800

    Update Q56MergeIntervals.java

--- Leetcode/src/array/Q56MergeIntervals.java
+++ Leetcode/src/array/Q56MergeIntervals.java
@@ -5,13 +5,14 @@ import java.util.List;
 
 public class Q56MergeIntervals {
 	
-	/*
+	/**
 	 * Method 1:
 	 * step 1: sort the intervals based on the 1st value
 	 * step 2: two parameters used,  	start: start value of current interval
 	 * 									end: end value of current interval
 	 * step 3: for each arr int[], compare arr[1] with end
-	 * 			if arr[1]> end, then (start, end) is one of the final intervals
+	 * 		   if arr[1]> end, then (start, end) is one of the final intervals
+	 *         else the current arr can be merged with (start, end)		   
 	 * 
 	 * Results: runtime 86ms beats 5.06% memory 40.8mb beats 93.74%
 	 * */
@@ -21,9 +22,9 @@ public class Q56MergeIntervals {
 		int start = intervals[0][0];
 		int end = intervals[0][1];
 		for (int i = 1; i < intervals.length; i++) {
-			if (intervals[i][0] > end) {
+			if (intervals[i][0] > end) { // then (start, end) is a final interval
 				res.add(new int[] { start, end });
-				start = intervals[i][0];
+				start = intervals[i][0]; // re-initial start & end
 				end = intervals[i][1];
 			} else if (intervals[i][0] <= end) {
 				if (intervals[i][1] > end)
@@ -41,6 +42,9 @@ public class Q56MergeIntervals {
 		return res2;
 	}
 
+	/**
+	 * Sort array.
+	 * */
 	private void sort(int[][] intervals) {
 		for (int i = 0; i < intervals.length - 1; i++) {
 			int min = i;
