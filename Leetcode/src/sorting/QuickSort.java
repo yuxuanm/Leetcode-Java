ca7ba57189652c60142f3596e1e6056b49a9b6b8
yuxuanm <48812435+yuxuanm@users.noreply.github.com>
2021-07-16 00:53:07 +0800

    add quicksort

---
+++ Leetcode/src/sorting/QuickSort.java
@@ -0,0 +1,41 @@
+package others;
+
+public class QuickSort {
+    public void sort(int[] arr){
+        quickSort(arr, 0, arr.length -1);
+
+    }
+
+    private void quickSort(int[] arr, int left, int right) {
+        if(left<right){
+            int index = partition(arr, left, right);
+            quickSort(arr, left, index-1);
+            quickSort(arr, index+1, right);
+        }
+    }
+
+    private int partition(int[] arr, int left, int right) {
+        int pivot = left;
+        int index = pivot +1;
+        for(int i = index; i<=right; i++){
+            if(arr[i]<arr[pivot]){
+                swap(arr, i, index++);
+            }
+        }
+        swap(arr, pivot, index-1);
+        return index-1;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        QuickSort quickSort = new QuickSort();
+        int [] arr = new int [] {5,3,9,16,2,7};
+        quickSort.sort(arr);
+        System.out.println(arr.toString());
+    }
+}
---
+++ Leetcode/src/sorting/README.md
@@ -0,0 +1,5 @@
+# Sorting
+| # | Title |
+| :-----:| :----: |
+|1|[QuickSort](https://github.com/yuxuanm/Leetcode-Java/blob/master/Leetcode/src/others/Q7ReverseInteger.java)|
+
